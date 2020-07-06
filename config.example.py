3ca0250e83592485a7517e6b8a84d14360fc1d45
marufraj32 <63406865+marufraj32@users.noreply.github.com>
2020-07-05 22:38:47 -0700

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 #
-# @name   : PhoneInfoga - Phone numbers OSINT tool
-# @url    : https://github.com/sundowndev
-# @author : Raphael Cerveaux (sundowndev)
+# @name   : FINDINFO- Phone numbers OSINT tool
+# @url    : https://github.com/marufraj32
+# @author : MARUF RAJ (TEAM 30)
 
 google_api_key=''
 google_cx_id=''
